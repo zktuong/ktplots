15f6bb49970bc4bec99e9ea99f6d910cbbb4b502
Kelvin <kt16@sanger.ac.uk>
2019-12-11 16:34:33 +0000

    update geneDotPlot

--- R/geneDotPlot.R
+++ R/geneDotPlot.R
@@ -52,7 +52,7 @@ geneDotPlot <- function(scdata, idents, genes, split.by = NULL, pct.threshold = 
     cat(paste0("attempting to subset the expression matrix to the ", length(genes), " genes provided"), sep = "\n")
     # expr_mat_filtered <- exp_mat[row.names(exp_mat) %in% genes, ]
     # exp_mat <- as.matrix(exp_mat)
-    expr_mat_filtered <- exp_mat[match(genes, row.names(exp_mat))[!is.na(match(genes, row.names(exp_mat)))], ]
+    expr_mat_filtered <- exp_mat[match(genes, row.names(exp_mat))[!is.na(match(genes, row.names(exp_mat)))], ,drop = FALSE]
 
     cat(paste0("found ", dim(expr_mat_filtered)[1], " genes in the expression matrix", sep ="\n"))
 
@@ -71,7 +71,7 @@ geneDotPlot <- function(scdata, idents, genes, split.by = NULL, pct.threshold = 
          
         meanExpr <- split(expr.df, expr.df$label)
         meanExpr <- lapply(meanExpr, function(x){
-            x <- x[,-1]
+            x <- x[,-1, drop = FALSE]
             x <- x %>% colMeans
             return(x)
         })
