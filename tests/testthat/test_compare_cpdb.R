4b07916047ba13f2856128eae311cb133b54fc2c
Kelvin <26215587+zktuong@users.noreply.github.com>
2022-04-06 18:25:58 +0100

    Update test_compare_cpdb.R

--- tests/testthat/test_compare_cpdb.R
+++ tests/testthat/test_compare_cpdb.R
@@ -14,6 +14,8 @@ test_that("compare_cpdb works 1", {
         groupby = "Status_on_day_collection_summary")
     expect_true(nrow(out[[1]]) == 8339)
     expect_true(ncol(out[[1]]) == 4)
+    expect_true(length(which(out[[1]]$padj < 0.05)) == 372)
+    expect_true(length(which(out[[1]]$pval < 0.05)) == 964)
 })
 
 
@@ -21,9 +23,11 @@ test_that("compare_cpdb works 2", {
     out <- compare_cpdb(cpdb_meta = covid_cpdb_meta, sample_metadata = covid_sample_metadata,
         celltypes = c("B_cell", "CD14", "CD16", "CD4", "CD8", "DCs", "MAIT", "NK_16hi",
             "NK_56hi", "Plasmablast", "Platelets", "Treg", "gdT", "pDC"), celltype_col = "initial_clustering",
-        groupby = "Status_on_day_collection_summary", method = "wilcox")
+        groupby = "Status_on_day_collection_summary", method = "wilcox.test")
     expect_true(nrow(out[[1]]) == 8339)
     expect_true(ncol(out[[1]]) == 4)
+    expect_true(length(which(out[[1]]$padj < 0.05)) == 289)
+    expect_true(length(which(out[[1]]$pval < 0.05)) == 941)
 })
 
 test_that("compare_cpdb works 3", {
@@ -31,6 +35,6 @@ test_that("compare_cpdb works 3", {
         celltypes = c("B_cell", "CD14", "CD16", "CD4", "CD8", "DCs", "MAIT", "NK_16hi",
             "NK_56hi", "Plasmablast", "Platelets", "Treg", "gdT", "pDC"), celltype_col = "initial_clustering",
         groupby = "Status_on_day_collection_summary", formula = "~ Status_on_day_collection_summary + (1|individual)",
-        method = "lme")
+        method = "lmer")
     expect_true(nrow(out) == 8339)
 })
