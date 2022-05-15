3431d027ae078ef5af38d660a63d267010e38485
Kelvin <26215587+zktuong@users.noreply.github.com>
2022-05-15 15:50:33 +0100

    Update test_compare_cpdb.R

--- tests/testthat/test_compare_cpdb.R
+++ tests/testthat/test_compare_cpdb.R
@@ -7,20 +7,20 @@ system("tar -xzf covid_cpdb.tar.gz")
 
 covid_sample_metadata$individual <- rep(c("A", "B", "C", "D", "E", "F"), 2)
 
-# test_that("compare_cpdb works 1", {
-#     out <- compare_cpdb(cpdb_meta = covid_cpdb_meta, sample_metadata = covid_sample_metadata,
-#         celltypes = c("B_cell", "CD14", "CD16", "CD4", "CD8", "DCs", "MAIT", "NK_16hi",
-#             "NK_56hi", "Plasmablast", "Platelets", "Treg", "gdT", "pDC"), celltype_col = "initial_clustering",
-#         groupby = "Status_on_day_collection_summary")
-#     p1 <- plot_compare_cpdb(out)
-#     p2 <- plot_compare_cpdb(out, cluster = TRUE, group = 'test')
-#     expect_true(nrow(out[[1]]) == 8339)
-#     expect_true(ncol(out[[1]]) == 5)
-#     expect_true(length(which(out[[1]]$padj < 0.05)) == 289)
-#     expect_true(length(which(out[[1]]$pval < 0.05)) == 941)
-#     expect_true(is.ggplot(p1))
-#     expect_true(is.ggplot(p2))
-# })
+test_that("compare_cpdb works 1", {
+    out <- compare_cpdb(cpdb_meta = covid_cpdb_meta, sample_metadata = covid_sample_metadata,
+        celltypes = c("B_cell", "CD14", "CD16", "CD4", "CD8", "DCs", "MAIT", "NK_16hi",
+            "NK_56hi", "Plasmablast", "Platelets", "Treg", "gdT", "pDC"), celltype_col = "initial_clustering",
+        groupby = "Status_on_day_collection_summary")
+    p1 <- plot_compare_cpdb(out)
+    p2 <- plot_compare_cpdb(out, cluster = TRUE, group = 'test')
+    expect_true(nrow(out[[1]]) == 8339)
+    expect_true(ncol(out[[1]]) == 5)
+    expect_true(length(which(out[[1]]$padj < 0.05)) == 289)
+    expect_true(length(which(out[[1]]$pval < 0.05)) == 941)
+    expect_true(is.ggplot(p1))
+    expect_true(is.ggplot(p2))
+})
 
 
 # test_that("compare_cpdb works 2", {
